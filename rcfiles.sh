23aa649206632513111fe1e17f51d7d612b5cefe
Kiran Madabhushi <maskiran@gmail.com>
2025-02-07 16:06:55 -0800

    read rcfiles from git instead of icloud

--- rcfiles.sh
+++ rcfiles.sh
@@ -3,10 +3,10 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 ICLOUD="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
 
-cp "$ICLOUD/rcfiles/zshrc" $HOME/.zshrc
+curl -o $HOME/.zshrc https://raw.githubusercontent.com/maskiran/setup-mac/refs/heads/main/zshrc
+curl -o $HOME/.oh-my-zsh/custom/themes/kiran.zsh-theme https://raw.githubusercontent.com/maskiran/setup-mac/refs/heads/main/kiran.zsh-theme
 cp "$ICLOUD/rcfiles/vimrc" $HOME/.vimrc
 cp "$ICLOUD/rcfiles/gitconfig" $HOME/.gitconfig
-cp "$ICLOUD/rcfiles/kiran.zsh-theme" $HOME/.oh-my-zsh/custom/themes/
 cp "$ICLOUD/rcfiles/aliases.zsh" $HOME/.oh-my-zsh/custom/
 
 mkdir $HOME/.ssh
